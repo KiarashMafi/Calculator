b9c1ecba678513b24d5a8f9ba1904077bdc02600
KiarashMafi <k.kiarashmafi@gmail.com>
2025-11-19 17:28:30 +0330

    functions added.

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,9 +1,7 @@
-// Calculator.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+	cout << "Hello, World!" << endl;
+	return 0;
 }
