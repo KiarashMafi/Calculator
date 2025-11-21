c84b4ae9d7748b8421593b48b5e076344f286a74
KiarashMafi <k.kiarashmafi@gmail.com>
2025-11-21 19:05:09 +0330

    selected_operation and while true added.

--- Calculator.cpp
+++ Calculator.cpp
@@ -29,13 +29,45 @@ int get_user_input() {
 	cout << "2) Subtraction (-)" << endl;
 	cout << "3) Multiplication (*)" << endl;
 	cout << "4) Division (/)" << endl;
+	cout << "5) Exit" << endl;
 	cout << "Enter the number of the operation (1-4): ";
 	cin >> input;
 	return input;
 }
 
+int get_numbers_input() {
+
+	float input1, input2;
+
+	cout << "Enter two numbers separated by space: ";
+	cin >> input1 >> input2;
+
+	return input1, input2;
+}
+
+int select_operation(int choice) {
+
+	float result{};
+
+	float num1{}, num2;
+	num1, num2 = get_numbers_input();
+
+	if (choice == 1) result = sum(num1, num2);
+	else if (choice == 2) result = subtraction(num1, num2);
+	else if (choice == 3) result = multiplication(num1, num2);
+	else if (choice == 4) result = division(num1, num2);
+
+	return result;
+}
+
 int main() {
 
-	string output = get_user_input();
+	while (true) {
+		int choose = get_user_input();
+		if (choose == 5) break;
 
+		float calculated_num = select_operation(choose);
+		cout << "Result: " << calculated_num << endl;
+
+	};
 }
